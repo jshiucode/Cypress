7fb095d6e09c151fbbb9bb39ab34d4915aa6d44f
Alex Abrams <alexabrams240@gmail.com>
2024-04-04 21:26:49 -0700

    Fixed request type

--- Gordian2 - Local/integration.py
+++ Gordian2 - Local/integration.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 from Gordian2 import Gordian
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def run_algorithm():
     (links, time, knots) = Gordian(request.data.decode("utf-8"))
     return {"links": links, "elapsed_time": time, "knots": knots}
