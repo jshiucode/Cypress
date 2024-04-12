a3e5e932335c43c5a8eba272856790618d75ed7c
Alex <68565327+Evelyn-0@users.noreply.github.com>
2024-04-11 17:46:22 -0700

    Update index.py

--- Gordian2 - Local/api/index.py
+++ Gordian2 - Local/api/index.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask import request, jsonify
 from flask_cors import CORS
-from Gordian2 import Gordian
+from algorithm import Gordian2
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=["POST"])
 def run_algorithm():
-    (links, time, knots) = Gordian(request.data.decode("utf-8"))
+    (links, time, knots) = Gordian2.Gordian(request.data.decode("utf-8"))
     return {"links": links, "elapsed_time": time, "knots": knots}
 
 @app.route("/", methods=["GET"])
