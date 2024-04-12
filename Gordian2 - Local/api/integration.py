182a7234fc72b883a0e6d74ffba02cf2976bea75
jshiu1 <jshiu@fhb.com>
2024-04-11 17:42:37 -0700

    new path added

--- Gordian2 - Local/api/integration.py
+++ Gordian2 - Local/api/integration.py
@@ -6,6 +6,10 @@ from Gordian2 import Gordian
 app = Flask(__name__)
 CORS(app)
 
+
+"""
+For running locally with React frontend (non-vercel)
+"""
 @app.route("/", methods=["POST"])
 def run_algorithm():
     (links, time, knots) = Gordian(request.data.decode("utf-8"))
