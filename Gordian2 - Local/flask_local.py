bfcfc629f115c8bba823fcf19c9e63200983ad00
jshiu1 <jshiu@fhb.com>
2024-04-16 15:11:43 -0700

    renamed to Cypress

--- Gordian2 - Local/flask_local.py
+++ Gordian2 - Local/flask_local.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-from algorithm.Gordian2 import Gordian
+from algorithm.Cypress import Cypress
 
 app = Flask(__name__)
 CORS(app)
@@ -12,5 +12,5 @@ For running locally with React frontend (non-vercel)
 """
 @app.route("/", methods=["POST"])
 def run_algorithm():
-    (links, time, knots) = Gordian(request.data.decode("utf-8"))
+    (links, time, knots) = Cypress(request.data.decode("utf-8"))
     return {"links": links, "elapsed_time": time, "knots": knots}
